75551fe443bf6bc7c6507e4bd728e46fd0a9b3cc
davdijaj <dxa6145@g.rit.edu>
2018-04-15 22:29:47 +0200

    Provided new GUI

---
+++ Ps3Preservation/src/ps3preservation/GUI.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ps3preservation;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Donat Avdijaj
+ */
+public class GUI extends JFrame {
+    
+    public GUI(String title) {
+        super(title);
+        
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
+
+        mainPanel.add(new SidePanel(), BorderLayout.WEST);
+        
+        JPanel centerPanel = new JPanel();
+        
+        centerPanel.setLayout(new BorderLayout());
+        
+        JPanel contentPanel = new JPanel();
+        
+        contentPanel.setLayout(new GridLayout(0, 1));
+        contentPanel.add(new CenterPanel("GAME_NAME", "placeholder.jpg"));
+        contentPanel.add(new CenterPanel("GAME_NAME2", "placeholder.jpg"));
+        contentPanel.add(new CenterPanel("GAME_NAME3", "placeholder.jpg"));
+        contentPanel.add(new CenterPanel("GAME_NAME4", "placeholder.jpg"));
+        contentPanel.add(new CenterPanel("GAME_NAME5", "placeholder.jpg"));
+        
+        centerPanel.add(new NorthPanel(), BorderLayout.NORTH);
+        centerPanel.add(contentPanel, BorderLayout.CENTER);
+        
+        mainPanel.add(centerPanel, BorderLayout.CENTER);
+        
+        add(mainPanel);
+        initializeMainFrame();
+    }
+    
+    public void setProperFrameSize() {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        
+        int height = screenSize.height * 2 / 3;
+        int width = screenSize.width * 2 / 3;
+        
+        setPreferredSize(new Dimension(width, height));
+    }
+    
+    public void initializeMenuBar() {
+        JMenuBar menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+        
+        JMenu mnFile = new JMenu("File");
+        menuBar.add(mnFile);
+        
+        JMenuItem mntmExit = new JMenuItem("Exit");
+        mntmExit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        mnFile.add(mntmExit);
+        JMenuItem mntmAbout = new JMenuItem("About");
+        JMenu mnAbout = new JMenu("About");
+        mnAbout.add(mntmAbout);
+        mntmAbout.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                
+            }
+        });
+        menuBar.add(mnAbout);
+    }
+    
+    public void initializeMainFrame() {
+        setProperFrameSize();
+        initializeMenuBar();
+        pack();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        setLocationRelativeTo(null);
+    }
+    
+    public static void main(String[] args) {
+        new GUI("PS3 Preservation Project");
+    }
+    
+}
