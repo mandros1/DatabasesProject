fc3a93cc5b904e913964007fbd7f0cf9097924a4
davdijaj <dxa6145@g.rit.edu>
2018-04-15 22:35:45 +0200

    Removed previous GUI, provided additions to the new GUI

---
+++ Ps3Preservation/src/ps3preservation/NorthPanel.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ps3preservation;
+
+import java.awt.Color;
+import java.awt.GridLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.colorchooser.AbstractColorChooserPanel;
+
+/**
+ *
+ * @author Donat Avdijaj
+ */
+public class NorthPanel extends JPanel{
+    
+    public NorthPanel(){
+        super();
+        setLayout(new GridLayout(1,3));
+        add(createSearchPanel());
+        add(createTitlePanel());
+        add(createUserPanel());
+    }
+    
+    public void setImageToLabel(JLabel label, String PicURL){
+        ImageIcon imgThisImg;
+        imgThisImg = new ImageIcon(PicURL);
+        label.setIcon(imgThisImg);
+    }
+    
+    public JPanel createSearchPanel(){
+        JLabel searchLabel = new JLabel("Search: ");
+        setImageToLabel(searchLabel, "src/media/search.png");
+        JTextField searchField = new JTextField(15);
+        
+        JPanel searchPanel = new JPanel();
+        searchPanel.add(searchLabel);
+        searchPanel.add(searchField);
+        
+        searchPanel.setBackground(Color.LIGHT_GRAY);
+        
+        return searchPanel;
+    }
+    
+    public JPanel createTitlePanel(){
+        JLabel titleLabel = new JLabel("PS3 Preservation Project");
+        
+        JPanel titlePanel = new JPanel();
+        titlePanel.add(titleLabel);
+        
+        titlePanel.setBackground(Color.LIGHT_GRAY);
+        
+        return titlePanel;
+    }
+    
+    public JPanel createUserPanel(){
+        JLabel userLabel = new JLabel("User");
+        setImageToLabel(userLabel, "src/media/user.png");
+        
+        JPanel userPanel = new JPanel();
+        userPanel.add(userLabel);
+        
+        userPanel.setBackground(Color.LIGHT_GRAY);
+        
+        return userPanel;
+    }
+    
+}
---
+++ Ps3Preservation/src/ps3preservation/SidePanel.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ps3preservation;
+
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Donat Avdijaj
+ */
+public class SidePanel extends JPanel{
+    
+    public SidePanel(){
+        super();
+        setBackground(Color.GRAY);
+        setLayout(new GridLayout(0,1));
+        JLabel menuLabel = new JLabel();
+        setImageToLabel(menuLabel, "src/media/menu.png");
+        
+        JLabel addLabel = new JLabel();
+        setImageToLabel(addLabel, "src/media/add.png");
+        
+        JLabel downloadLabel = new JLabel();
+        setImageToLabel(downloadLabel, "src/media/download.png");
+        
+        add(menuLabel);
+        add(addLabel);
+        add(downloadLabel);
+    }
+    
+    public void setImageToLabel(JLabel label, String PicURL){
+        ImageIcon imgThisImg;
+        imgThisImg = new ImageIcon(PicURL);
+        label.setIcon(imgThisImg);
+    }
+    
+}
